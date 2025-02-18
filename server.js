5985aa50e609613625dd09804b16f4dfdfe1f81f
Jaclyn McKeown <ronanmckeown86@gmail.com>
2025-02-19 00:30:37 +1000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("", (req, res) => {
+    res.send("idiot");
+})
+
+app.listen(port, () => {
+    console.log(`server hosted on ${port}`);
+})
