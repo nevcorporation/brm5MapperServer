230460b7f235d5dd95b0fe53f171902c724c3b52
Jaclyn McKeown <ronanmckeown86@gmail.com>
2025-02-19 22:39:56 +1000

    Updated server

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const messages = [];
+const messages = [
+    {
+        "code":"0000",
+        "texts": [
+            
+        ]
+    }
+];
 
 app.use(cors());
 
